fa35487f9851aa2829500fe35024bf1cb4484e17
Holly-Wigmore <165459716+Holly-Wigmore@users.noreply.github.com>
2024-08-24 14:44:02 +0800

    Added weather data to weekly forecast

--- src/script.js
+++ src/script.js
@@ -57,6 +57,11 @@ function useSearchInput(event) {
   searchCity(searchInput.value);
 }
 
+function getDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
 function getForecast(city) {
   let apiKey = "754b84e4ec96a5481tfao024db3b3aff";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`;
@@ -67,17 +72,23 @@ function displayForecast(response) {
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">☀️</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${getDay(day.time)}</div>
+            <img class="weather-forecast-icon" src="${day.condition.icon_url}"/>
             <div class="weather-forecast-temperatures">
-              <div class="weather-forecast-temp"><strong>15°C</strong></div>
-              <div class="weather-forecast-temp">12°C</div>
+              <div class="weather-forecast-temp"><strong>${Math.round(
+                day.temperature.maximum
+              )}</strong></div>
+              <div class="weather-forecast-temp">${Math.round(
+                day.temperature.minimum
+              )}</div>
             </div>
           </div>`;
+    }
   });
   let dailyForecast = document.querySelector("#daily-forecast");
   dailyForecast.innerHTML = forecastHtml;
